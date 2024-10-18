09c365e4cef7deecf087f4323623f4d6b6a056c1
MoonBurst <Moonburstplays@gmail.com>
2024-10-18 04:26:45 -0500

    Update config.h

--- keyboards/hotdox76v2/keymaps/vial/config.h
+++ keyboards/hotdox76v2/keymaps/vial/config.h
@@ -1,3 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 #pragma once
 #define VIAL_KEYBOARD_UID {0x0A, 0x8C, 0x44, 0x78, 0x90, 0x37, 0x3A, 0xCA}
+#define VIAL_UNLOCK_COMBO_ROWS {10,10,8,10}
+#define VIAL_UNLOCK_COMBO_COLS {1,2,5,3}
